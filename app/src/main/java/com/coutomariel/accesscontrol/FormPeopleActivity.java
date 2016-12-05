a205a077feaeef18b471cef5962732f736055277
coutomariel <coutomariel@gmail.com>
2016-12-05 17:20:02 -0200

    Criados métodos de carregamento do enum para os spinners de setor e tipo de pessoa

--- app/src/main/java/com/coutomariel/accesscontrol/FormPeopleActivity.java
+++ app/src/main/java/com/coutomariel/accesscontrol/FormPeopleActivity.java
@@ -5,36 +5,47 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.coutomariel.accesscontrol.model.Setor;
+import com.coutomariel.accesscontrol.model.TipoPessoa;
+
 import java.util.ArrayList;
 
 public class FormPeopleActivity extends AppCompatActivity {
+    Spinner spinnerTpPessoa;
+    Spinner spinnerSetores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_people);
 
-        Spinner spinnerTpPessoa = (Spinner) findViewById(R.id.spnTipoPessoa);
+        spinnerTpPessoa = (Spinner) findViewById(R.id.spnTipoPessoa);
+        this.carregaTiposPessoa();
 
+
+        spinnerSetores = (Spinner) findViewById(R.id.spnSetor);
+        this.carregaSetores();
+
+
+    }
+
+    private void carregaTiposPessoa(){
         ArrayList<String> tiposPessoa = new ArrayList<>();
-        tiposPessoa.add("Funcionário");
-        tiposPessoa.add("Fornecedor");
-        tiposPessoa.add("Cliente");
-        tiposPessoa.add("Visitante");
+        for(TipoPessoa pessoa : TipoPessoa.values() ){
+            tiposPessoa.add(pessoa.getDescricao());
+        }
         ArrayAdapter adapterTpPessoas = new ArrayAdapter(FormPeopleActivity.this, android.R.layout.simple_spinner_item, tiposPessoa);
         adapterTpPessoas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerTpPessoa.setAdapter(adapterTpPessoas);
+    }
 
-        Spinner spinnerSetores = (Spinner) findViewById(R.id.spnSetor);
+    private void carregaSetores(){
         ArrayList<String> setores = new ArrayList<>();
-        setores.add("Administrativo");
-        setores.add("Armazém");
-        setores.add("Entrega");
-        setores.add("Puxada");
-        setores.add("Vendas");
+        for(Setor setor : Setor.values()){
+            setores.add(setor.getDescricao());
+        }
         ArrayAdapter adapterSetores = new ArrayAdapter(FormPeopleActivity.this, android.R.layout.simple_spinner_item, setores);
         adapterSetores.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerSetores.setAdapter(adapterSetores);
-
     }
 }
