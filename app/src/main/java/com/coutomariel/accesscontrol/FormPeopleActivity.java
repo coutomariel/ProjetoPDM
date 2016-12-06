1f7f3b9682a05680ed8038f7445f48667a51e762
coutomariel <coutomariel@gmail.com>
2016-12-06 16:43:56 -0200

    #5 - Incluído date picker para atributo data de admissão

--- app/src/main/java/com/coutomariel/accesscontrol/FormPeopleActivity.java
+++ app/src/main/java/com/coutomariel/accesscontrol/FormPeopleActivity.java
@@ -1,19 +1,27 @@
 package com.coutomariel.accesscontrol;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.coutomariel.accesscontrol.com.coutomariel.accesscontrol.fragment.DatePickerFragment;
 import com.coutomariel.accesscontrol.model.Setor;
 import com.coutomariel.accesscontrol.model.TipoPessoa;
 import com.coutomariel.accesscontrol.repository.PeopleRepository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class FormPeopleActivity extends AppCompatActivity {
-    Spinner spinnerTpPessoa;
-    Spinner spinnerSetores;
+
+    private Spinner spinnerTpPessoa, spinnerSetores;
+
+    private EditText edtAdmissao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,28 @@ public class FormPeopleActivity extends AppCompatActivity {
 
 
     }
+
+    public void setData(View view) {
+        DatePickerFragment datePickerFragment = new DatePickerFragment();
+
+        Calendar cal = Calendar.getInstance();
+
+        Bundle bundle = new Bundle();
+        bundle.putInt("dia", cal.get(Calendar.DAY_OF_MONTH));
+        bundle.putInt("mes", cal.get(Calendar.MONTH));
+        bundle.putInt("ano", cal.get(Calendar.YEAR));
+
+        datePickerFragment.setArguments(bundle);
+        datePickerFragment.setDateListener(dateListener);
+        datePickerFragment.show(getFragmentManager(), "Data Adissão.");
+    }
+
+    private DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            edtAdmissao.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+        }
+    };
 
     private void carregaTiposPessoa(){
         ArrayList<String> tiposPessoa = new ArrayList<>();
---
+++ app/src/main/java/com/coutomariel/accesscontrol/com/coutomariel/accesscontrol/fragment/DatePickerFragment.java
@@ -0,0 +1,36 @@
+package com.coutomariel.accesscontrol.com.coutomariel.accesscontrol.fragment;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+/**
+ * Created by Mariel on 06/12/2016.
+ */
+
+public class DatePickerFragment extends DialogFragment{
+
+    private DatePickerDialog.OnDateSetListener listener;
+
+    private int dia, mes, ano;
+
+    public DatePickerFragment() {}
+
+    public void setDateListener(DatePickerDialog.OnDateSetListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void setArguments(Bundle args) {
+        super.setArguments(args);
+        dia = args.getInt("dia");
+        mes = args.getInt("mes");
+        ano = args.getInt("ano");
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new DatePickerDialog(getActivity(), listener, ano, mes, dia);
+    }
+}
