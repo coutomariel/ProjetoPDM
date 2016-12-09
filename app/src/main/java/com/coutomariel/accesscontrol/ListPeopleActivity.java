40a77bc568dbffac3572420408ee543797ea80da
coutomariel <coutomariel@gmail.com>
2016-12-09 16:01:13 -0200

    #9 - Disponibilizado botão para adicionar registro a partir da activity de listage

--- app/src/main/java/com/coutomariel/accesscontrol/ListPeopleActivity.java
+++ app/src/main/java/com/coutomariel/accesscontrol/ListPeopleActivity.java
@@ -1,8 +1,11 @@
 package com.coutomariel.accesscontrol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.coutomariel.accesscontrol.model.Pessoa;
@@ -15,6 +18,7 @@ public class ListPeopleActivity extends AppCompatActivity {
 
     private Pessoas repository;
     private ListView lstPessoas;
+    private Button addPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,7 @@ public class ListPeopleActivity extends AppCompatActivity {
 
         repository = new Pessoas(this);
         lstPessoas = (ListView) findViewById(R.id.lstPeople);
+
 
         List<Pessoa> lista = repository.listarPessoas();
 
@@ -36,6 +41,14 @@ public class ListPeopleActivity extends AppCompatActivity {
 
         lstPessoas.setAdapter(adapter);
 
+        addPessoa = (Button) findViewById(R.id.btnAddPeople);
+        addPessoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ListPeopleActivity.this, FormPeopleActivity.class );
+                startActivity(intent);
+            }
+        });
 
     }
 }
